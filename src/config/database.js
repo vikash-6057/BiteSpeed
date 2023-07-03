88f3e5cac8d77e48ef81f7afef1352888297046c
Vikash Kumar <vikash.kumar.186057@gmail.com>
2023-07-03 22:07:13 +0530

    mysql database added

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+
+const dbName = process.env.dbName
+const dbUser = process.env.dbUser
+const dbHost = process.env.dbHost
+const dbDriver = process.env.dbDriver
+const dbPassword = process.env.dbPassword
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+    host: dbHost,
+    dialect: dbDriver,
+    logging: false
+});
+
+exports.sequelize = sequelize;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 require('dotenv').config();
 const { router: router } = require('./router')
+const { sequelize: sequelize } = require('./config/database')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(router);
-
+sequelize.sync()
+    .then(() => console.log('Connected to database'))
+    .catch((error) => console.error(error))
 app.listen(PORT, () => {
     console.log(`Server started on PORT : ${PORT}`);
 })
