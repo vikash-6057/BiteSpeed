d63d8d8a49560372da92ec3187fd7deb3b7e8e20
Vikash Kumar <vikash.kumar.186057@gmail.com>
2023-07-04 15:48:46 +0530

    table added

---
+++ src/modules/contact/model/contact_model.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require("sequelize");
+const { linkPrecedence } = require('../../../constants/enum_constants');
+const { sequelize } = require("../../../config/database");
+const Contact = sequelize.define(
+    'Contact',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+        },
+        phoneNumber: {
+            type: DataTypes.STRING,
+        },
+        linkedId: {
+            type: DataTypes.STRING
+        },
+        linkPrecedence: {
+            type: DataTypes.ENUM,
+            values: [linkPrecedence.PRIMARY, linkPrecedence.SECONDARY],
+            defaultValue: linkPrecedence.PRIMARY
+
+        },
+        deletedAt: {
+            type: DataTypes.DATE
+        }
+    }
+)
+Contact.hasMany(Contact, {
+    foreignKey: 'linkedId',
+    as: 'linkedContact'
+});
+Contact.belongsTo(Contact, {
+    foreignKey: 'linkedId',
+    as: 'parentContact'
+});
+exports.Contact = Contact;
