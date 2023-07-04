ae0235d86f90ed809162908e2383e5d6f613b78a
Vikash Kumar <vikash.kumar.186057@gmail.com>
2023-07-04 17:16:55 +0530

    controller added

--- src/modules/contact/controller/contact_controller.js
+++ src/modules/contact/controller/contact_controller.js
@@ -1,14 +1,14 @@
 const { Router } = require('express');
-
+const contactService = require('../service/contact_service');
+const { formatResponse } = require('../../../utils/utils');
 const router = Router();
-
 router.post('/identify', async (req, res, next) => {
     try {
-        const { phone, email } = req.body
-        console.log(phone, email);
-        res.send('success').status(200);
+        const contact = await contactService.identify(req.body);
+        const response = formatResponse(contact)
+        res.send(response).status(200);
     } catch (error) {
-        console.error(error);
+        res.status(400).send(error.message);
     }
 })
 exports.router = router;
